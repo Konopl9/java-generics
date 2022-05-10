020b481aeed71ef8a8c852be20fa0d47b1d050e1
mishcma <maksym.mishchenko@storaenso.com>
2022-05-10 13:12:43 +0200

    bg-10 added array list, liked list and performance test

---
+++ collections-framework/src/com/mishcma/list/ArrayListMain.java
@@ -0,0 +1,38 @@
+package com.mishcma.list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListMain {
+
+    public static void main(String[] args) {
+        // resizing the array takes 0(N)
+        List<String> names = new ArrayList<>(500000000);
+
+        names.add("Kevin");
+        names.add("Max");
+        names.add("Maksym");
+
+        // random indexing in O(1)
+        System.out.println(names.get(0));
+
+        //insert item into a given index - O(N)
+        names.add(0, "Daniel");
+        names.remove(0);
+
+        // contains in array O(N)
+        System.out.println(names.contains("Adam"));
+
+        Object[] o = names.toArray();
+
+        for (Object obj :
+            o) {
+            System.out.println(obj);
+        }
+
+        // because of the Iterable
+        for (String s : names) {
+            System.out.println(s);
+        }
+    }
+}
---
+++ collections-framework/src/com/mishcma/list/LinkedListMain.java
@@ -0,0 +1,50 @@
+package com.mishcma.list;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class LinkedListMain {
+
+    public static void main(String[] args) {
+        List<Integer> list = new LinkedList<>();
+
+        list.add(1);
+        list.add(3);
+        list.add(5);
+        list.add(11);
+
+        for (Integer num : list) {
+            System.out.println(num);
+        }
+
+        // Usually we use linked list (MORE MEMORY !!!)
+        LinkedList<Integer> linkedList = new LinkedList<>();
+        linkedList.addFirst(1);
+        linkedList.addLast(2);
+
+        // TEST
+        ArrayList<Integer> arrayList = new ArrayList<>();
+
+        long now = System.currentTimeMillis();
+
+        for (int i = 0; i < 100000; i++) {
+            arrayList.add(i);
+        }
+
+        System.out.println("Time taken for ArrayList: " + (System.currentTimeMillis() - now));
+
+        LinkedList<Integer> linkedList1 = new LinkedList<>();
+        now = System.currentTimeMillis();
+
+        for (int i = 0; i < 100000; i++) {
+            linkedList1.addFirst(i);
+        }
+
+        System.out.println("Time taken for LinkedList: " + (System.currentTimeMillis() - now));
+
+        // CONCLUSION: Linked List has better performance if we need to insert the value at first
+        // position because it requires only reference update without shifting with O(N)
+
+    }
+}
