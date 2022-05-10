64d1c0b12b8250979b3b97d7ed9bc67ba1239dbd
mishcma <maksym.mishchenko@storaenso.com>
2022-05-10 14:54:14 +0200

    bg-11 stack

---
+++ collections-framework/src/com/mishcma/list/StackMain.java
@@ -0,0 +1,24 @@
+package com.mishcma.list;
+
+import java.util.Stack;
+
+public class StackMain {
+
+    public static void main(String[] args) {
+        Stack<String> stack = new Stack<>();
+
+        stack.push("Adam");
+        stack.push("Adam2");
+        stack.push("Adam1");
+        stack.push("Kailey");
+        stack.push("ALena");
+
+        System.out.println(stack.size());
+        System.out.println(stack.pop());
+        System.out.println(stack.size());
+
+        while (!stack.isEmpty()) {
+            System.out.println(stack.pop());
+        }
+    }
+}
